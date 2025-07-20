36fc7ec9dea734191582990fa35f8f02752c8eca
Sergei Belov <sergei-belov@wow-play.com>
2025-07-20 16:34:41 +0300

    Add the precompiled header file with C++ 20 features.

---
+++ Source/PCH.h
@@ -0,0 +1,134 @@
+// Copyright ❤️ 2025, Wow Play
+
+// C++ 20 Standard Library headers
+#pragma once
+
+// Multi-purpose headers
+#include <cstdlib>
+#include <execution>
+
+// Language support library
+#include <cfloat>
+#include <climits>
+#include <compare>
+#include <coroutine>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdint>
+#include <exception>
+#include <initializer_list>
+#include <limits>
+#include <new>
+#include <source_location>
+#include <typeindex>
+#include <typeinfo>
+#include <version>
+
+// Concepts library
+#include  <concepts>
+
+// Diagnostics library
+#include <cassert>
+#include <cerrno>
+#include <stdexcept>
+#include <system_error>
+
+// Memory management library
+#include <memory>
+#include <memory_resource>
+#include <scoped_allocator>
+
+// Metaprogramming library
+#include <ratio>
+#include <type_traits>
+
+// General utilities library
+#include <any>
+#include <bit>
+#include <bitset>
+#include <functional>
+#include <optional>
+#include <tuple>
+#include <utility>
+#include <variant>
+
+// Containers library
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <span>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+// Iterators library
+#include <iterator>
+
+// Ranges library
+#include <ranges>
+
+// Algorithms library
+#include <algorithm>
+#include <numeric>
+
+// Strings library
+#include <cstring>
+#include <string>
+#include <string_view>
+
+// Text processing library
+#include <cctype>
+#include <charconv>
+#include <clocale>
+#include <cuchar>
+#include <cwchar>
+#include <cwctype>
+#include <format>
+#include <locale>
+#include <regex>
+
+// Numerics library
+#include <cfenv>
+#include <cmath>
+#include <complex>
+#include <numbers>
+#include <random>
+#include <valarray>
+
+// Time library
+#include <chrono>
+#include <ctime>
+
+// Input/output library
+#include <cinttypes>
+#include <cstdio>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <streambuf>
+#include <syncstream>
+
+// Concurrency support library
+#include <atomic>
+#include <barrier>
+#include <condition_variable>
+#include <future>
+#include <latch>
+#include <mutex>
+#include <semaphore>
+#include <shared_mutex>
+#include <stop_token>
+#include <thread>
